19737e7fd4e7d63b3cf4311413b24daf2d031d8c
Quentin Chouleur <qchouleur@pobox.com>
2015-02-21 17:29:53 +0100

    Extracted pricing rule from ParkingMeter

--- ParkingMeter.Logic/ParkingMeter.cs
+++ ParkingMeter.Logic/ParkingMeter.cs
@@ -1,19 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace ParkingMeter.Logic
+﻿namespace ParkingMeter.Logic
 {
+    using System;
+    using System.Linq;
+
     public class ParkingMeter
     {
         private readonly decimal[] _validCoins = { 0.2m, 0.5m, 1m, 2m };
         
         public decimal TotalAmountOfMoney { get; private set; }
+
+        private readonly PrincingRule _pricingRule;
         private readonly IClock _clock;
 
         public ParkingMeter(IClock clock)
         {
             _clock = clock;
+            _pricingRule = new PrincingRule();
         }
 
         public void AddCoin(decimal coinValue)
@@ -28,44 +30,7 @@ namespace ParkingMeter.Logic
 
         public DateTime GetParkingLimitDate()
         {
-            return _clock.Now + ComputeLimitTime();
+            return _clock.Now + _pricingRule.CalculateTimeLimitForAmount(TotalAmountOfMoney);
         }
-
-        private readonly Dictionary<decimal, TimeSpan> _pricingRules = new Dictionary<decimal, TimeSpan>()
-        {
-            {1m, new TimeSpan(0, 20, 0)},
-            {2m, new TimeSpan(1, 0, 0)},
-            {3m, new TimeSpan(2, 0, 0)},
-            {5m, new TimeSpan(12, 0, 0)},
-            {8m, new TimeSpan(24, 0, 0)}
-        };
-
-        private TimeSpan ComputeLimitTime()
-        {
-            var minimalAmount = _pricingRules.Keys.Min();
-            
-            if (TotalAmountOfMoney < minimalAmount)
-            {
-                return new TimeSpan(0);
-            }
-
-            var remaining = TotalAmountOfMoney;
-            var totalTimeLimit = new TimeSpan(0);
-            while (remaining >= minimalAmount)
-            {
-                foreach (var possibleAmount in _pricingRules.Keys.OrderByDescending(price => price))
-                {
-                    if (remaining >= possibleAmount)
-                    {
-                        totalTimeLimit = totalTimeLimit + _pricingRules[possibleAmount];
-                        remaining -= possibleAmount;
-                        break;
-                    }
-                }
-            }
-
-            return totalTimeLimit;
-        }
-
     }
 }
---
+++ ParkingMeter.Logic/PrincingRule.cs
@@ -0,0 +1,39 @@
+﻿namespace ParkingMeter.Logic
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class PrincingRule
+    {
+        private readonly Dictionary<decimal, TimeSpan> _pricingRules = new Dictionary<decimal, TimeSpan>()
+        {
+            {1m, new TimeSpan(0, 20, 0)},
+            {2m, new TimeSpan(1, 0, 0)},
+            {3m, new TimeSpan(2, 0, 0)},
+            {5m, new TimeSpan(12, 0, 0)},
+            {8m, new TimeSpan(24, 0, 0)}
+        };
+
+        public TimeSpan CalculateTimeLimitForAmount(decimal amount)
+        {
+            var minimalAmount = _pricingRules.Keys.Min();
+
+            if (amount < minimalAmount)
+            {
+                return new TimeSpan(0);
+            }
+
+            var remaining = amount;
+            var totalTimeLimit = new TimeSpan(0);
+            while (remaining >= minimalAmount)
+            {
+                var maximumPossibleAmount = _pricingRules.Keys.Where(price => remaining >= price).Max();
+                totalTimeLimit += _pricingRules[maximumPossibleAmount];
+                remaining -= maximumPossibleAmount;
+            }
+
+            return totalTimeLimit;
+        }
+    }
+}
